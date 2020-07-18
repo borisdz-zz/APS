97dfff579ae4bcdd4f95899560887bc141a4f722
Boris Dzotov <borisdzotov@gmail.com>
2020-07-18 17:43:31 +0200

    18.7.2020 Pred pocnuvanje so vezbanje

---
+++ src/Stack_Zagradi.java
@@ -0,0 +1,16 @@
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.Scanner;
+//import java.util.Stack;
+//
+//public class Stack_Zagradi {
+//    public static void main(String[] args) {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Stack stek = new Stack();
+//
+//        String izraz = "s*(s-a)*(s-b)*(s-c)";
+//        char i;
+//        while()
+//
+//    }
+//}
